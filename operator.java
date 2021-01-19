63b66b24e03bd638a0d556d3c751a669c4d7b3b7
Sid <44680199+jainsiddharth99@users.noreply.github.com>
2021-01-19 12:05:34 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,7 @@
+class Operator {
+    public static void main(String[] args){
+          int var=5;    
+          System.out.println(var++);          
+          System.out.println("\n"+ ++var);    
+         }
+    }
