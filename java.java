7df5fcccb665a36b39661ca6afa3ce5e84007cb8
Sid <44680199+jainsiddharth99@users.noreply.github.com>
2021-02-01 13:59:33 +0530

    Add files via upload

---
+++ java.java
@@ -0,0 +1,5 @@
+class java{
+ public static void main(String args[]){
+     System.out.println("Hello World! i am learning java.");
+ }
+}
---
+++ large.java
@@ -0,0 +1,21 @@
+class large
+{
+    public static void main(String args[])
+        {
+            int a=Integer.parseInt(args[0]);
+            int b=Integer.parseInt(args[1]);
+            int c=Integer.parseInt(args[2]);    
+            if(a>b && a>c)
+                {
+                    System.out.println("A Is Largest Number!...");
+                }
+            else if(b>a && b>c)
+                {
+                    System.out.println("B Is Largest number!...");
+                }
+            else    
+                {
+                    System.out.println("C Is Largest number!...");
+                }
+        }
+}
